8773b3d774f5e5b47973e5e8c103f9a074d3d6c3
Nizami Jussupov <jussupov.02@gmail.com>
2022-08-31 17:30:59 +0600

    First commit

---
+++ main.py
@@ -0,0 +1,20 @@
+import telebot
+from decouple import config
+
+
+API_KEY = config('TOKEN')
+
+bot = telebot.TeleBot(API_KEY)
+
+
+@bot.message_handler(commands=['start'])
+def start_command(message):
+    bot.send_message(message.chat.id, "Hello, I am Echo bot")
+
+
+@bot.message_handler(func=lambda message: True)
+def message_handler(message):
+    bot.send_message(message.chat.id, message.text)
+
+
+bot.polling()
